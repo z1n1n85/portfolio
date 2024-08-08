c5a49ed577f3dbdc4a09311effd4e10276049b3f
Zinin Ilya <metal-fox8@yandex.ru>
2024-08-08 16:52:01 +0300

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  output: 'export',
+  webpack: (cfg) => {
+      cfg.module.rules.push(
+          {
+              test: /\.md$/,
+              loader: 'frontmatter-markdown-loader',
+              options: { mode: ['react-component'] }
+          }
+      )
+      return cfg
+  }
+}
